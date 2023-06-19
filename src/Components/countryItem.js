59113b407c5dd370273183e2e3c14a64f627be01
Stensis <irenenjuguna98@gmail.com>
2023-06-20 01:48:28 +0300

    defined the CountryItemView component

--- src/Components/countryItem.js
+++ src/Components/countryItem.js
@@ -1,3 +1,15 @@
 import React from 'react'; 
 import  Styles from './countryItem.module.css'
 
+export function CountryItemView({
+    name,
+    flags,
+    callingCodes,
+    region,
+    subregion,
+    population,
+    area,
+    timezones,
+  }) {
+
+  }
