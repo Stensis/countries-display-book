c59b2f7172230b1db6beba5326c444d91bd34e6a
Stensis <irenenjuguna98@gmail.com>
2023-06-20 02:02:50 +0300

    fetched data and setting its  staates

--- src/Components/countryData.js
+++ src/Components/countryData.js
@@ -2,7 +2,38 @@ import React, { useState, useEffect } from "react";
 import { CountryItemView } from "./countryItem";
 import Styles from "./countryData.module.css";
 
-function countryData(){
+const API_URL = "https://restcountries.com/v2/all";
 
+
+function countryData() {
+    const [countryData, setCountryData] = useState([]);
+    const [originalData, setOriginalData] = useState([]);
+    const [hasFetchError, setHasFetchError] = useState(false);
+    
+    useEffect(() => {
+        let isMounted = true;
+    
+        fetch(API_URL)
+          .then(async (res) => {
+            const dataArr = await res.json();
+            if (isMounted) {
+              setIsFetching(false);
+              setHasFetchError(false);
+              setCountryData(dataArr);
+              setOriginalData(dataArr);
+            }
+          })
+          .catch((_err) => {
+            if (isMounted) {
+              setIsFetching(false);
+              setHasFetchError(true);
+            }
+          });
+    
+        return () => {
+          isMounted = false;
+        };
+      }, []);
+    
 }
 export default countryData;
