6d945184f1f8537b8576f02b8fc092e77434d2a2
Stensis <irenenjuguna98@gmail.com>
2023-06-20 02:08:24 +0300

    implemented the handle filter functionality

--- src/Components/countryData.js
+++ src/Components/countryData.js
@@ -5,14 +5,19 @@ import Styles from "./countryData.module.css";
 const API_URL = "https://restcountries.com/v2/all";
 const ASC = "Asc";
 const DESC = "Desc";
+const FILTER_TYPE_LITHUANIA = "filter-lithuania";
+const FILTER_TYPE_OCEANIA = "filter-oceania";
 
 function countryData() {
+  // setting variables states
   const [countryData, setCountryData] = useState([]);
   const [originalData, setOriginalData] = useState([]);
   const [hasFetchError, setHasFetchError] = useState(false);
   const [isFetching, setIsFetching] = useState(true);
   const [sortType, setSortType] = useState(undefined);
+  const [filterType, setFilterType] = useState(undefined);
 
+  // fetching the data from  the api server
   useEffect(() => {
     let isMounted = true;
 
@@ -37,7 +42,7 @@ function countryData() {
       isMounted = false;
     };
   }, []);
-
+  // sorting the country data by ascending and descending order
   function handleSortData(type) {
     setSortType(type);
     if (type === ASC) {
@@ -64,6 +69,24 @@ function countryData() {
       setCountryData(sorted);
     }
   }
-
+  //   filtering function to filter countrie in ocenia region and that are small than luthuania
+  function handleFilterData(type) {
+    setFilterType(type);
+    if (type === FILTER_TYPE_LITHUANIA) {
+      const lithuania = originalData.find(
+        (country) => country.name === "Lithuania"
+      );
+      const filterSize = lithuania ? lithuania.area : 0;
+      const filtered = [...originalData].filter(
+        (country) => country.area < filterSize
+      );
+      setCountryData(filtered);
+    } else if (type === FILTER_TYPE_OCEANIA) {
+      const filtered = [...originalData].filter(
+        (country) => country.region === "Oceania"
+      );
+      setCountryData(filtered);
+    }
+  }
 }
 export default countryData;
